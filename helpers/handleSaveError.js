601eb3062d9f31d342fe141fdb2a04943fc2540d
lastavchuk <it.lastavchuk@gmail.com>
2023-10-14 22:56:52 +0300

    feat: added error status 409

--- helpers/handleSaveError.js
+++ helpers/handleSaveError.js
@@ -2,6 +2,7 @@ const handleSaveError = (err, data, next) => {
     err.status = 400;
 
     if (err.code === 11000) {
+        err.status = 409;
         err.message = `The user with this ${
             Object.keys(err.keyValue)[0]
         } is already registered`;
